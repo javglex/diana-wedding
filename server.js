8bf0ca36aa00c1e7b66d332dd0c0007adc9fefbd
javier gonzalez <javglex@yahoo.com>
2018-04-22 14:52:44 -0700

    another attempt to fix routing issue

--- server.js
+++ server.js
@@ -8,9 +8,6 @@ var router = express.Router();
 // Run the app by serving the static files in the dist directory
 app.use(express.static(__dirname + '/dist'));
 
-// Start the app by listening on the default Heroku port
-app.listen(port);
-
 // For all GET requests, send back index.html so that PathLocationStrategy can be used
 app.get('/*', function(req, res) {
   res.sendFile(path.join(__dirname + '/dist/index.html'));

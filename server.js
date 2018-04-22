3b346ebf0680de270800c57a2cc3cbde7e494b54
javier gonzalez <javglex@yahoo.com>
2018-04-22 14:31:13 -0700

    added fix to 404 routing error

--- server.js
+++ server.js
@@ -4,8 +4,16 @@ const app = express();
 var router = express.Router();
 
 //avoid 404 error when navigating to different routes from browser
+// Run the app by serving the static files in the dist directory
+app.use(express.static(__dirname + '/dist'));
+
+// Start the app by listening on the default Heroku port
+app.listen(port);
+
+// For all GET requests, send back index.html so that PathLocationStrategy can be used
 app.get('/*', function(req, res) {
-    res.sendFile(path.join(__dirname + '/dist/index.html'));
-  });
+  res.sendFile(path.join(__dirname + '/dist/index.html'));
+});
+
 // Start the app by listening on the default Heroku port
 app.listen(process.env.PORT || 8080);
